af6a9c7ffde5ad576bec183939f1ffdde8c93781
Michael Wingfield <michael.wingfield@twcable.com>
2015-08-28 09:32:55 -0600

    got most of the stuff drawn

--- js/main.js
+++ js/main.js
@@ -14,7 +14,6 @@ window.onload = function () {
     };
     document.addEventListener('keydown', function (event) {
         var k = event.keyCode;
-        console.log(event.keyCode);
         if (k == 38 || k == 87){
             keyState.up = 1;
         } else if (k == 39 || k == 68){
@@ -27,7 +26,6 @@ window.onload = function () {
     });
     document.addEventListener('keyup', function(event) {
         var k = event.keyCode;
-        console.log(event.keyCode);
         if (k == 38 || k == 87){
             keyState.up = 0;
         } else if (k == 39 || k == 68){
@@ -45,46 +43,182 @@ window.onload = function () {
         lastKeyState.down = keyState.down;
     };
 
-    var objs = [];
-    var a = new Obj(0, 0, 100, 100, "blue");
-    a.update = function(){
+    var neutralZones = [];
+    neutralZones.push(new NeutralZone());
+    neutralZones.push(new Flag(0));
+    neutralZones.push(new Flag(1));
+    var team0 = [];
+    team0.push(new Player(50, 50, 0, 1));
+    team0[0].update = function(){
         this.x = mouse.x;
         this.y = mouse.y;
     };
-    objs.push(a);
-    var a = new Obj(0, 0, 100, 100, "red");
-    a.speed = 10;
-    a.update = function(){
-        this.x += (keyState.right ? this.speed : keyState.left ? -this.speed : 0);
-        this.y += (keyState.down ? this.speed : keyState.up ? -this.speed : 0);
+    var team1 = [];
+    team1.push(new Player(100, 100, 1, 1));
+    team1[0].update = function(){
+        this.x += (keyState.left ? -10 : (keyState.right ? 10 : 0));
+        this.y += (keyState.up ? -10 : (keyState.down ? 10 : 0));
     }
-    objs.push(a);
+    var updatables = [];
+    team0.forEach(function(obj){updatables.push(obj);});
+    team1.forEach(function(obj){updatables.push(obj);});
+    var drawables = [];
+    neutralZones.forEach(function(obj){drawables.push(obj);});
+    updatables.forEach(function(obj){drawables.push(obj);});
 
     setInterval(function(){
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.fillStyle = "#33CC33";
+        ctx.fillRect(0, 0, canW, canH);
+
+        updatables.forEach(function(obj){if (obj.update){obj.update();}});
+
+        drawables.forEach(function(obj){obj.draw();});
+
+        if (keyState.space && !lastKeyState.space){
+            team0.forEach(function(a){
+                team1.forEach(function(b){
+                    if (a.contains(b)){
+                        console.log(a + " contains " + b);
+                    }
+                })
+            });
+        }
         updateKeyState();
-
-        objs.forEach(function(i, obj){
-            i.update();
-        });
-
-        objs.forEach(function(i, obj){
-            i.draw();
-        });
     }, 1000 / 30);
 };
 
-function Obj(x, y, w, h, col){
+function dst(x1, y1, x2, y2){
+    return Math.sqrt((x2 - x1)*(x2 - x1) + (y2 - y1) * (y2 - y1));
+}
+
+function diff(x1, y1, x2, y2){
+    return {x: x1 - x2, y: y1 - y2};
+}
+
+//function norm(x, y){
+//    var len = dst(0, 0, x, y);
+//    return (len > 0 ? {x: x / len, y: y / len} : {x:0, y:0});
+//}
+
+function Player(x, y, team, number){
     this.x = x || 0;
     this.y = y || 0;
-    this.w = w || 0;
-    this.h = h || 0;
-    this.col = col || "#000000";
+    this.isOut = 0;
+    this.path = [];
+    this.maxPathDist = canW * 0.1;
+    this.maxPathLen = 2;
+    this.r = canW * 0.025;
+    this.team = team;
+    this.number = number;
+    this.col = (team ? "#3399FF" : "#FF5050");
+    this.outln = (team ? "#0033CC" : "#CC0000");
     this.draw = function(){
-        if (this.w > 0 && this.h > 0) {
-            ctx.fillStyle = this.col;
-            ctx.fillRect(this.x - this.w / 2, this.y - this.h / 2, this.w, this.h);
+        ctx.fillStyle = this.col;
+        ctx.strokeStyle = this.outln;
+        ctx.lineWidth = 5;
+        ctx.setLineDash([1, 0]);
+        ctx.beginPath();
+        ctx.arc(this.x, this.y,this.r,0,2*Math.PI);
+        ctx.fill();
+        ctx.stroke();
+        ctx.closePath();
+    }
+    this.targetDiff = {x: 0, y: 0};
+    this.startingPoint = {x: 0, y: 0};
+    this.calcNextMove = function(){
+        if (this.path.length > 0){
+            this.startingPoint = {x: this.x, y: this.y};
+            this.targetDiff = diff(this.path[0].x, this.path[0].y, this.x, this.y);
         }
     }
-    this.update = function(){};
+    this.moveToTarget = function(percentThere){
+        this.x = this.startingPoint.x + this.targetDiff.x * percentThere;
+        this.y = this.startingPoint.y + this.targetDiff.y * percentThere;
+    };
+    this.contains = function(player){
+        if (player.team == this.team){
+            return false;
+        } else {
+            var d = dst(player.x, player.y, this.x, this.y);
+            return (d < (this.r + player.r));
+        }
+    };
+    this.toString = function(){
+        return "[" + this.team + ":" + this.number + "]"
+    };
+}
+
+function NeutralZone(){
+    this.percentW = canW * 0.05;
+    this.center = canW / 2;
+    this.left = this.center - this.percentW;
+    this.right = this.center + this.percentW;
+    this.draw = function(){
+        ctx.lineWidth = 0;
+        ctx.fillStyle = "#999999";
+        ctx.fillRect(this.left, 0, this.percentW * 2, canH);
+
+        ctx.lineWidth = 5;
+        ctx.strokeStyle = "#666666";
+        ctx.setLineDash([10,5]);
+
+        ctx.beginPath();
+        ctx.moveTo(this.left, 0);
+        ctx.lineTo(this.left, canH);
+        ctx.moveTo(this.right, 0);
+        ctx.lineTo(this.right, canH);
+        ctx.stroke();
+        ctx.closePath();
+    }
+
+    this.contains = function(player){
+        return (player.x > this.left && player.x < this.right);
+    }
+}
+
+function Flag(team){
+    this.x = (team ? canW * 0.05 : canW * 0.95);
+    this.y = canH / 2;
+    this.carried = 0;
+    this.r  = canW * 0.07;
+    this.team = team;
+    this.flagSize = canW * 0.01;
+    this.flagCol = (team ? "#3399FF" : "#FF5050");
+    this.flagOutln = (team ? "#0033CC" : "#CC0000");
+    this.col = "#999999";
+    this.outln = "#666666";
+    this.draw = function(){
+        if (!this.carried) {
+            ctx.fillStyle = this.col;
+            ctx.strokeStyle = this.outln;
+            ctx.lineWidth = 5;
+            ctx.setLineDash([10, 7]);
+            ctx.beginPath();
+            ctx.arc(this.x, this.y, this.r, 0, 2 * Math.PI);
+            ctx.fill();
+            ctx.stroke();
+            ctx.closePath();
+        }
+
+        ctx.fillStyle = this.flagOutln;
+        ctx.strokeStyle = this.flagCol;
+        ctx.lineWidth = 1;
+        ctx.setLineDash([1, 0]);
+        ctx.beginPath();
+        ctx.moveTo(this.x, this.y);
+        ctx.lineTo(this.x - this.flagSize, this.y - this.flagSize);
+        ctx.lineTo(this.x + this.flagSize, this.y - this.flagSize);
+        ctx.lineTo(this.x, this.y);
+        ctx.fill();
+        ctx.stroke();
+        ctx.closePath();
+    }
+    this.contains = function(player){
+        if (this.carried || this.team == player.team){
+            return false;
+        } else {
+            var d = dst(player.x, player.y, this.x, this.y);
+            return (d < this.r);
+        }
+    }
 }
