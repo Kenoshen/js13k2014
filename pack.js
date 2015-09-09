d58c02bddbba73480ede2e4c653dcbfb4b8bd22c
Michael Wingfield <michael.wingfield@twcable.com>
2015-09-08 21:10:13 -0600

    got package script working, does a minify and then zips the directory.  output is MUCH smaller than 13kb, more like 7kb, phewf!

---
+++ pack.js
@@ -0,0 +1,50 @@
+var gameName = "capthatflag"
+
+var minify = require("minify")
+var fs = require('fs.extra');
+var EasyZip = require('easy-zip').EasyZip;
+var zip = new EasyZip();
+fs.mkdir("tmp", function(err){
+    fs.mkdir("tmp/" + gameName, function(err){
+        fs.mkdir("tmp/" + gameName + "/game", function(err){
+            minify('game/main.js', function (error, data) {
+                if (error)
+                    console.error(error.message);
+                else {
+                    fs.writeFile("tmp/" + gameName + "/game/main.js", data, function (err) {
+                        if (err) {
+                            return console.log(err);
+                        }
+
+                        console.log("The file was saved!");
+                        minify('game/srv.js', function (error, data) {
+                            if (error)
+                                console.error(error.message);
+                            else {
+                                fs.writeFile("tmp/" + gameName + "/game/srv.js", data, function (err) {
+                                    if (err) {
+                                        return console.log(err);
+                                    }
+
+                                    console.log("The file was saved!");
+                                    fs.copy('game.json', 'tmp/' + gameName + '/game.json', { replace: true }, function (err) {
+                                        fs.copy('game/index.html', 'tmp/' + gameName + '/game/index.html', { replace: true }, function (err) {
+                                            fs.copy('game/package.json', 'tmp/' + gameName + '/game/package.json', { replace: true }, function (err) {
+                                                var zip5 = new EasyZip();
+                                                zip5.zipFolder('tmp/' + gameName,function(){
+                                                    zip5.writeToFile('tmp/' + gameName + '.zip');
+                                                });
+                                            });
+                                        });
+                                    });
+                                });
+                            }
+                        });
+                    });
+                }
+            });
+        });
+    });
+});
+
+
