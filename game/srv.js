bfa91670642b281774de5a95a4fbf299440e2b66
Michael Wingfield <michael.wingfield@twcable.com>
2015-09-08 19:30:57 -0600

    trying to get it in the correct format for the server stuff.

---
+++ game/srv.js
@@ -0,0 +1,9 @@
+log('Hi! This is some game.');
+
+var socketio = require('sandbox-io');
+log('Loaded sandbox-io', socketio);
+
+socketio.on('connection', function(socket){
+  socket.on('event', function(data){});
+  socket.on('disconnect', function(){});
+});
